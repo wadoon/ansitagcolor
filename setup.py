baaaed237272aa92464ed252dbefe1d984406fea
Alexander Weigl <spamling@web.de>
2014-05-04 19:24:24 +0200

    add enum requirement

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
     author="Alexander Weigl",
     author_email="alexweigl@gmail.com",
     py_modules=["ansi"],
+    requires=['enum'],
     classifiers=[
         'License :: OSI Approved :: MIT License',
         "Environment :: Console",
